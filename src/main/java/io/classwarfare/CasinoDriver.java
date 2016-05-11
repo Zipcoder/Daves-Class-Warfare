c8e04527c5b51f616f7827c85a2f1db1bbe728a2
Zihao Castine <zihaocastine@Zipcoder02s-MacBook-Pro.local>
2016-05-11 18:38:44 -0400

    add RussianRoulette:

--- src/main/java/io/classwarfare/CasinoDriver.java
+++ src/main/java/io/classwarfare/CasinoDriver.java
@@ -1,17 +1,41 @@
 package io.classwarfare;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zihaocastine on 5/11/16.
  */
 public class CasinoDriver {
+    RussianRoulette russianRoulette;
+    Player player;
     public static void main(String[] args) {
         CasinoDriver casinoDriver=new CasinoDriver();
-        casinoDriver.printLogo();
-        
+        casinoDriver.start();
+
+    }
+    private void start(){
+        /**
+         * Make new object of Player and games
+         */
+        player=new Player();
+
+        Blackjack blackjack=new Blackjack();
+        SlotMachine slotMachine=new SlotMachine();
+        russianRoulette=new RussianRoulette(player);
+
+        Scanner input=new Scanner(System.in);
+        String choice="";
+        printLogo();
+        System.out.println("Welcome to Great Wall Casino \nPlease choice a game\n"+
+                "1)Blackjack\n2)Slot Machine\n3)Hangman");
+        choiceGame();
+
     }
 
+    /**
+     * print the wall on to the console.
+     */
     private void printLogo(){
         delayOutput("  _______ .______       _______      ___   .___________.   ____    __    ____  ___       __       __       ");
         delayOutput(" /  _____||   _  \\     |   ____|    /   \\  |           |   \\   \\  /  \\  /   / /   \\     |  |     |  |       ");
@@ -42,6 +66,11 @@ public class CasinoDriver {
         }
 
     }
+
+    /**
+     * Delay the output by 100 milliseconds
+     * @param s
+     */
     private void delayOutput(String s){
         try {
             TimeUnit.MILLISECONDS.sleep(100);
@@ -49,5 +78,25 @@ public class CasinoDriver {
             e.printStackTrace();
         }
         System.out.println(s);
+    }
+
+    private boolean choiceGame(){
+        Scanner input=new Scanner(System.in);
+        String choice="";
+        System.out.print("Enter: ");
+        choice=input.next();
+        switch (choice){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":russianRoulette.play();
+            default:
+                break;
+
+        }
+        return false;
     }
 }
