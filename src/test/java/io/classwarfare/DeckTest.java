20b6827eaaf895e46f506f19c168a56ef3f43e3e
Brian Abbott <brianabbott@Zipcoder25s-MacBook-Pro.local>
2016-05-11 13:47:07 -0400

    Added Deck class

--- src/test/java/io/classwarfare/DeckTest.java
+++ src/test/java/io/classwarfare/DeckTest.java
@@ -1,20 +1,27 @@
 package io.classwarfare;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by brianabbott on 5/11/16.
  */
 public class DeckTest {
 
-    @Test
-    public void createDeckTest(){
-
+    @Test   //New Deck should have 52 cards added by the addsuit method in the constructor
+    public void deckDefaultSizeTest(){
+        Deck deckSuitTest = new Deck();
+        int expectedDeckSize = 52;
+        int actualDeckSize = deckSuitTest.cards.size();
+        assertEquals("The deck should have 52 cards",expectedDeckSize,actualDeckSize);
     }
 
     @Test
     public void shuffleTest(){
-
+        Deck deckShuffledTest = new Deck();
+        Card expectedFirstCard = deckShuffledTest.cards.get(0);
+        deckShuffledTest.shuffle();
+        Card actualFirstCard = deckShuffledTest.cards.get(0);
+        assertNotEquals("The two decks should be different",expectedFirstCard,actualFirstCard);
     }
-
 }
