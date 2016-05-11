6e002a24a35e1bc161b5f0e6ed37ccaf5925e111
Zihao Castine <zihaocastine@Zipcoder02s-MacBook-Pro.local>
2016-05-11 13:31:16 -0400

    add Player

--- src/main/java/io/classwarfare/Deck.java
+++ src/main/java/io/classwarfare/Deck.java
@@ -7,17 +7,5 @@ import java.util.ArrayList;
  * Created by brianabbott on 5/11/16.
  */
 public class Deck {
-    ArrayList<Card> cards = new ArrayList<Card>();
 
-
-    Deck(){
-
-        for(int i = 0; i < 12; i++){
-    }
-
-    public void shuffle(){
-
-    }
-
-    public void createDeck(){}
 }
--- src/main/java/io/classwarfare/Player.java
+++ src/main/java/io/classwarfare/Player.java
@@ -6,5 +6,26 @@ package io.classwarfare;
 public class Player {
     private double wallet;
     private int playId;
+    private double bet;
+    private static int counter=1;
+
+    //set player balance to 5000, and Id;
+    Player(){
+        wallet=5000;
+        playId=counter;
+        bet=0;
+        counter++;
+    }
+    public void placeBet(double bet){
+        this.wallet-=bet;
+    }
+
+    public double showBalance(){
+        return wallet;
+    }
+
+    public void collectWinnings(double amount){
+        this.wallet+=amount;
+    }
 
 }
