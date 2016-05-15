d1e3e8bd94e3dc14020fe9967071bc4bcaf1efd2
Wesley Traute <wesleytraute@Zipcoder20s-MacBook-Pro.local>
2016-05-15 16:19:21 -0400

    modificed blackjack comments to follow scheme

--- src/main/java/io/classwarfare/Blackjack.java
+++ src/main/java/io/classwarfare/Blackjack.java
@@ -28,7 +28,7 @@ public class Blackjack extends Game {
         input = new Scanner(System.in);
         gameInSession = true;
         Player player = new Player();
-        while (gameInSession && player.showBalance()>=0) {
+        while (gameInSession && player.getWallet()>=0) {
 
             deck= new Deck();
             playerHand = new Hand();
@@ -59,7 +59,10 @@ public class Blackjack extends Game {
         }
     }
 
-
+    /**
+     *           PROMPTS THE USER TO HIT OR STAY
+     * @param input
+     */
     public void chooseHitOrStay(Scanner input){
         System.out.println("Press 1 for Hit, 2 for Stay");
         int hitOrStay = input.nextInt();
@@ -78,8 +81,12 @@ public class Blackjack extends Game {
         }
     }
 
+    /**
+     *         ASKS THE USER IF THEY WOULD LIKE TO PLAY ANOTHER ROUND
+     * @param input
+     */
     public void askToPlayAgain(Scanner input){
-        System.out.println("Your new balance is " + player.showBalance() + " Would you like to play again? 1 for Yes, 2 for No");
+        System.out.println("Your new balance is " + player.getWallet() + " Would you like to play again? 1 for Yes, 2 for No");
         int playAgain = input.nextInt();
         if(playAgain==1 && gameInSession){
             placeBet();
@@ -91,6 +98,9 @@ public class Blackjack extends Game {
         }
     }
 
+    /**
+     *            COMPARES HANDS TO CHOOSE A WINNER
+     */
     public void decideWinner(){
         if (winningHand == playerHand) {
             System.out.println("You Win!");
@@ -107,7 +117,9 @@ public class Blackjack extends Game {
 
     }
 
-    // checkBust checks to see if a hand is above 21 and returns a boolean-false by default.
+    /**
+     *     CHECKS TO SEE IF A HAND IS ABOVE 21
+     */
     public boolean checkIfHandIsOver21(Hand hand) {
         boolean isBust = false;
         if (hand.checkValue() > 21) {
@@ -130,8 +142,11 @@ public class Blackjack extends Game {
         return bustCheck;
     }
 
-
-    // checkBlackJack checks to see if a hand is at 21 and returns a boolean-false by default.
+    /**
+     *     CHECKS FOR BLACKJACK.
+     * @param hand
+     * @return
+     */
     public boolean checkHandForBlackJack(Hand hand) {
         boolean handHasBlackJack = false;
         if (hand.checkValue() == 21){
@@ -139,16 +154,14 @@ public class Blackjack extends Game {
         }
         return handHasBlackJack;
     }
-    public boolean checkForBlackJack(){
 
+    public boolean checkForBlackJack(){
         if ((checkHandForBlackJack(dealerHand) && checkHandForBlackJack(playerHand)) || checkHandForBlackJack(dealerHand)) {
             System.out.println("Dealer wins by Blackjack!");
-           // winningHand = dealerHand;
             hitAndStayPhase = false;
             return hitAndStayPhase;
         }else if (checkHandForBlackJack(playerHand) && !checkHandForBlackJack(dealerHand)) {
             System.out.println("You win by Blackjack!");
-            //winningHand = playerHand;
             player.collectWinnings((player.getBet() * 1.5));
             hitAndStayPhase = false;
             return hitAndStayPhase;
@@ -157,7 +170,11 @@ public class Blackjack extends Game {
         }
 
     }
-    // compareHands compares the two hands, and will return the higher hand. Dealer wins on a tie.
+
+    /**
+     *     COMPARES HANDS AND RETURNS THE HIGHER HAND
+     * @return
+     */
     public Hand compareHands() {
         Hand winningHand = dealerHand;
         if (playerHand.checkValue() > dealerHand.checkValue()) {
@@ -166,7 +183,9 @@ public class Blackjack extends Game {
         return winningHand;
     }
 
-    // Deal method is used to give the player and the dealer two cards each.
+    /**
+     *     DEALS CARDS
+     */
     public void deal() {
         playerHand.cardList.add(deck.cards.get(cardsPulledFromDeck));
         cardsPulledFromDeck++;
@@ -178,13 +197,18 @@ public class Blackjack extends Game {
         cardsPulledFromDeck++;
     }
 
-    // hit method adds a card to the Hand that is passed into it.
+    /**
+     *     ADDS CARD TO THE HAND THAT IS PASSED
+     * @param hand
+     */
     public void hit(Hand hand) {
         hand.cardList.add(deck.cards.get(cardsPulledFromDeck));
         cardsPulledFromDeck++;
     }
 
-    // prints the First card of the dealer, and all the cards in the players hand.
+    /**
+     *     PRINTS BLACKJACK GRAPHICS
+     */
     public void showHands(){
         System.out.println("Dealer showing card is \n" + dealerHand.cardList.get(0).getGraphic() + "\n\nYour hand is: ");
         for(int i =0;i<playerHand.cardList.size();i++){
@@ -193,6 +217,9 @@ public class Blackjack extends Game {
         System.out.println(" for a total of "+ playerHand.checkValue());
     }
 
+    /**
+     *    PROMPTS THE USER TO PLACE A BET
+     */
     private void placeBet(){
             double bet=0;
 
